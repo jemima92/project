592766ef9d6aa13f93508f4e60ad1009d6a1762c
jemima92 <161817635+jemima92@users.noreply.github.com>
2024-06-17 00:25:27 +0200

    Update register.go

--- templates/register.go
+++ templates/register.go
@@ -25,10 +25,10 @@
                     <a class="nav-link" href="/">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/categories">Categories</a>
+                    <a class="nav-link" href="categories.html">Categories</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Login</a>
+                    <a class="nav-link" href="login.html">Login</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link active" href="/register">Register</a>
